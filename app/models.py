e265569aa9f07ef0e8c23dd5d5f1de7fb9df2f96
LuisMiguelPoveda <lm.poveda@alumnos.upm.es>
2025-12-10 12:17:18 +0100

    basic login implementation

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
